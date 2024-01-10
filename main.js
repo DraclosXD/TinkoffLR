929348679e92479775406cce6c387f46d8e3b9e2
DraclosXD <yura.yurov.y@gmail.com>
2024-01-10 21:00:49 +0400

    new branch

---
+++ main.js
@@ -0,0 +1,5 @@
+const button = document.getElementById('buttonMore');
+const sectionSix = document.getElementById('section-sixth');
+button.addEventListener('click', () =>
+  sectionSix.scrollIntoView({behavior: 'smooth'})
+);
